33b1bf86f035c8a3c6fdf93469ef8150c0ac0ad3
KronosDev-Pro <KronosDev-Pro@users.noreply.github.com>
2020-08-11 00:11:02 +0200

    first push

---
+++ Version.md
@@ -0,0 +1,19 @@
+# KronosDev - MeteoWeb V 1.0.0
+## V 1.0.0
+
+### [client-side]
+- Basic Style & script
+- Framework (Bootstrap & Jquery)
+- Responsive App
+
+### [server-side]
+- Use expressjs & https (for the server)
+- tls/ssl secure connection
+
+## V 1.1.0
+### [client-side]
+- use Kronos - Framework
+- retire all dependance with old framework
+- round efficiencies Coordinate Service
+### [server-side]
+- view for the upgrade security App
---
+++ server.js
@@ -0,0 +1,51 @@
+let cors = require('cors');
+let helmet = require('helmet');
+let cookieParser = require('cookie-parser');
+let parser = require('body-parser');
+let morgan = require('morgan');
+let express = require('express');
+let app = express();
+let session = require('express-session');
+let path = require('path');
+let fs = require('fs');
+let https = require('https');
+require('dotenv').config();
+
+var privateKey = fs.readFileSync('KronosDev.dev-key.pem', 'utf8');
+var certificate = fs.readFileSync('KronosDev.dev.pem', 'utf8');
+
+var credentials = { key: privateKey, cert: certificate };
+
+app.set('trust proxy', 1);
+app.use(session({
+    secret: 'KronosDevPro',
+    name: 'SessionID',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true, maxAge: 60000 }
+}));
+app.use(morgan('combined'));
+app.use(parser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+    if (req.headers.host === '127.0.0.1') res.redirect('https://kronos.dev/');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/env', (req, res) => {
+    console.log(req.body);
+    if (req.body.list === 'API_OWM_*') {
+        res.send({ 'API_OWM_KEY': process.env.API_OWM_KEY, 'API_OWM_LINK': process.env.API_OWM_LINK, 'API_OWM_LINKUV': process.env.API_OWM_LINKUV });
+    }
+});
+
+app.get('/dev', (req, res) => {});
+
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(process.env.PORT_LISTEN, process.env.HOST_LISTEN);
