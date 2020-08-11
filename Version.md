985e06d32fde5034e705fd6c1c4932701b168c6b
KronosDev-Pro <kronosdev.pro@gmail.com>
2020-08-12 00:27:44 +0200

    Fisrt Update

--- Version.md
+++ Version.md
@@ -1,19 +1,21 @@
-# KronosDev - MeteoWeb V 1.0.0
-## V 1.0.0
+# KronosDev - MeteoWeb V 1.1.0
+## V 1.0.0 (release)
 
 ### [client-side]
-- Basic Style & script
-- Framework (Bootstrap & Jquery)
-- Responsive App
+- Basic Style & script ✔️
+- Framework (Bootstrap & Jquery) ✔️
+- Responsive App ✔️
 
 ### [server-side]
-- Use expressjs & https (for the server)
-- tls/ssl secure connection
+- Use expressjs & https (for the server) ✔️
+- tls/ssl secure connection ✔️
 
-## V 1.1.0
+## V 1.1.0 (development)
 ### [client-side]
-- use Kronos - Framework
-- retire all dependance with old framework
-- round efficiencies Coordinate Service
+- use Kronos - Framework ⚒
+- retire all dependance with old framework ⚒
+- round efficiencies Coordinate Service ✔️
+- Change Style / Design ⚒
 ### [server-side]
-- view for the upgrade security App
+- view for the upgrade security App ⚒
+- Send view with ejs to client ✔️
--- server.js
+++ server.js
@@ -9,7 +9,15 @@ let session = require('express-session');
 let path = require('path');
 let fs = require('fs');
 let https = require('https');
+let ejs = require('ejs');
 require('dotenv').config();
+
+var options = {
+    html: {
+        removeAttributeQuotes: false,
+        removeOptionalTags: false
+    },
+};
 
 var privateKey = fs.readFileSync(`${process.env.HOST_LISTEN}-key.pem`, 'utf8');
 var certificate = fs.readFileSync(`${process.env.HOST_LISTEN}.pem`, 'utf8');
@@ -25,24 +33,34 @@ app.use(session({
     cookie: { secure: true, maxAge: 60000 }
 }));
 app.use(morgan('combined'));
-app.use(parser.urlencoded({ extended: false }));
+app.use(parser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
 app.disable('x-powered-by');
 app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, 'assets')));
+app.use('/public', express.static(path.join(__dirname, 'assets/public')));
+app.set('views', './assets/public/view');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    if (req.headers.host === '127.0.0.1') res.redirect(`https://${process.env.HOST_LISTEN}/`);
-    res.sendFile(__dirname + '/index.html');
+    if (req.headers.host === '127.0.0.1' || req.headers.host === 'localhost') res.redirect(`https://${process.env.HOST_LISTEN}/`);
+    res.sendFile(__dirname + '/assets/public/index.min.html');
 });
 
 app.post('/env', (req, res) => {
-    console.log(req.body);
+    console.log(req.body, 'env');
     if (req.body.list === 'API_OWM_*') {
         res.send({ 'API_OWM_KEY': process.env.API_OWM_KEY, 'API_OWM_LINK': process.env.API_OWM_LINK, 'API_OWM_LINKUV': process.env.API_OWM_LINKUV });
     }
+});
+
+app.post('/meteo', (req, res) => {
+    res.render('meteo', { 'data': req.body.data, 'dataCountry': req.body.DataJsonCountry, 'UV': req.body.UV });
+});
+
+app.post('/notif', (req, res) => {
+    res.render('notif', { 'type': req.body.type });
 });
 
 app.get('/dev', (req, res) => {});
